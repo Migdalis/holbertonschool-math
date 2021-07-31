6e632b4e4a75334695dfc2c8084063ad710feb2a
Migdalis <miglepage@gmail.com>
2021-07-30 22:39:00 -0500

    Display Complex numbers

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -14,6 +14,10 @@ void display_complex_number(complex c)
                         printf("%.0f\n", c.re);
                 else if (c.im == 1)
                         printf("%.0f + i\n", c.re);
+                else if (c.im == -1)
+                        printf("%.0f - i\n", c.re);
+                else if (c.im < 0)
+                        printf("%.0f - %.0fi\n", c.re, c.im);
                         else
                                 printf("%.0f + %.0fi\n", c.re, c.im);
         }
